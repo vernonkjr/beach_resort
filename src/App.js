afef0dabd3909179e56dee5e2a8c45498912e242
vernonkjr <vkerrjr@gmail.com>
2019-10-25 17:04:51 -0400

    add Navbar component stub

--- src/App.js
+++ src/App.js
@@ -8,15 +8,18 @@ import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
 import Error from './pages/Error';
 
+import Navbar from './components/Navbar';
+
 function App() {
   return (
     <>
-    <Switch>
-      <Route exact path='/' component={Home} />
-      <Route exact path='/rooms/' component={Rooms} />
-      <Route exact path='/rooms/:slug' component={SingleRoom} />
-      <Route component={Error} />
-    </Switch>    
+      <Navbar />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/rooms/' component={Rooms} />
+        <Route exact path='/rooms/:slug' component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>    
     </>
   );
 }
