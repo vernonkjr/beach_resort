d6f3e368c4852caebfe6fec19bc635020b333ecc
vernonkjr <vkerrjr@gmail.com>
2019-11-06 16:53:54 -0500

    create Hero component, add it to Home, Rooms, Error

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,9 @@
-import React from 'react'
+import React from 'react';
+import Hero from '../components/Hero';
 
 const Home = () => {
     return (
-        <div>
-            hello from home page
-        </div>
+        <Hero hero='defaultHero'></Hero>
     )
 }
 
